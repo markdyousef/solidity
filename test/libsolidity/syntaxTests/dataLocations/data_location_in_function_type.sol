2b76750cb8e3da0e4e6cbe5c2732fd441c68dd3b
chriseth <chris@ethereum.org>
2018-08-08 14:46:42 +0200

    Update our own tests.

--- test/libsolidity/syntaxTests/dataLocations/data_location_in_function_type.sol
+++ test/libsolidity/syntaxTests/dataLocations/data_location_in_function_type.sol
@@ -1,11 +1,4 @@
 library L {
     struct Nested { uint y; }
-    // data location specifier in function signature should be optional even if there are multiple choices
-    function b(function(Nested calldata) external returns (uint)[] storage) external pure {}
     function c(function(Nested memory) external returns (uint)[] storage) external pure {}
-    function d(function(Nested storage) external returns (uint)[] storage) external pure {}
 }
-
-// ----
-// TypeError: (173-179): Data location must be "memory" for parameter in function, but "calldata" was given.
-// TypeError: (357-363): Data location must be "memory" for parameter in function, but "storage" was given.
